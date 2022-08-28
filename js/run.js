d46207f337b510c367d84e5f27a7b7f3e37a249d
Manu <90511900+Manumanoharan@users.noreply.github.com>
2022-08-28 23:16:38 +0530

    Add files via upload

---
+++ js/run.js
@@ -0,0 +1,12 @@
+function addElement () {
+  
+    const newDiv = document.createElement("div");
+  
+    
+    const newContent = document.createTextNode("Hi there and greetings!");
+  
+    newDiv.appendChild(newContent);
+  
+   
+    const currentDiv = document.getElementById("div1");
+    document.body.insertBefore(newDiv, currentDiv)}
